0e2dcf730868ab1ce44c2da46c184872bdb6769b
Jing Xie <xjing76@gmail.com>
2020-06-25 14:37:52 -0400

    allow for interger mu in simulate_poiszero_hmm and reformat according to balck

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -12,58 +12,61 @@ def test_sampling(N: int = 200, off_param=1):
     simulation, _ = simulate_poiszero_hmm(**kwargs)
 
     with pm.Model() as test_model:
-        p_0_rv = pm.Dirichlet('p_0', np.r_[1, 1, 1])
-        p_1_rv = pm.Dirichlet('p_1', np.r_[1, 1, 1])
-        p_2_rv = pm.Dirichlet('p_2', np.r_[1, 1, 1])
+        p_0_rv = pm.Dirichlet("p_0", np.r_[1, 1, 1])
+        p_1_rv = pm.Dirichlet("p_1", np.r_[1, 1, 1])
+        p_2_rv = pm.Dirichlet("p_2", np.r_[1, 1, 1])
 
         P_tt = tt.stack([p_0_rv, p_1_rv, p_2_rv])
-        P_rv = pm.Deterministic('P_tt', P_tt)
+        P_rv = pm.Deterministic("P_tt", P_tt)
 
-        pi_0_tt = simulation['pi_0']
-        y_test = simulation['Y_t']
+        pi_0_tt = simulation["pi_0"]
+        y_test = simulation["Y_t"]
 
-        S_rv = HMMStateSeq('S_t', y_test.shape[0], P_rv, pi_0_tt)
+        S_rv = HMMStateSeq("S_t", y_test.shape[0], P_rv, pi_0_tt)
         S_rv.tag.test_value = (y_test > 0).astype(np.int)
 
-        mu_1, mu_2 = kwargs['mus']
+        mu_1, mu_2 = kwargs["mus"]
 
         E_1_mu, Var_1_mu = mu_1 * off_param, mu_1 / 5
         E_2_mu, Var_2_mu = (mu_2 - mu_1) * off_param, (mu_2 - mu_1) * 0.9
 
-        mu_1_rv = pm.Gamma('mu_1', E_1_mu ** 2 / Var_1_mu, E_1_mu / Var_1_mu)
-        mu_2_rv = pm.Gamma('mu_2', E_2_mu ** 2 / Var_2_mu, E_2_mu / Var_2_mu)
+        mu_1_rv = pm.Gamma("mu_1", E_1_mu ** 2 / Var_1_mu, E_1_mu / Var_1_mu)
+        mu_2_rv = pm.Gamma("mu_2", E_2_mu ** 2 / Var_2_mu, E_2_mu / Var_2_mu)
 
-        Y_rv = SwitchingProcess('Y_t',
-                                [pm.Constant.dist(0),
-                                 pm.Poisson.dist(mu_1_rv),
-                                 pm.Poisson.dist(mu_1_rv + mu_2_rv)],
-                                S_rv, observed=y_test)
+        Y_rv = SwitchingProcess(
+            "Y_t",
+            [
+                pm.Constant.dist(0),
+                pm.Poisson.dist(mu_1_rv),
+                pm.Poisson.dist(mu_1_rv + mu_2_rv),
+            ],
+            S_rv,
+            observed=y_test,
+        )
 
     with test_model:
         mu_step = pm.NUTS([mu_1_rv, mu_2_rv])
         ffbs = FFBSStep([S_rv])
         transitions = TransMatConjugateStep([p_0_rv, p_1_rv, p_2_rv], S_rv)
-        steps = [
-            ffbs,
-            mu_step,
-            transitions
-        ]
+        steps = [ffbs, mu_step, transitions]
         start_time = datetime.now()
         trace_ = pm.sample(N, step=steps, return_inferencedata=True, chains=1)
         time_elapsed = datetime.now() - start_time
-        y_trace = pm.sample_posterior_predictive(trace_.posterior)['Y_t'].mean(axis=0)
+        y_trace = pm.sample_posterior_predictive(trace_.posterior)["Y_t"].mean(axis=0)
 
-    st_trace = trace_.posterior['S_t'].mean(axis=0).mean(axis=0)
-    mean_error_rate = (1 - np.sum(np.equal(st_trace, simulation['S_t']) * 1) / len(simulation['S_t'])).values.tolist()
+    st_trace = trace_.posterior["S_t"].mean(axis=0).mean(axis=0)
+    mean_error_rate = (
+        1 - np.sum(np.equal(st_trace, simulation["S_t"]) * 1) / len(simulation["S_t"])
+    ).values.tolist()
 
-    positive_index = simulation['Y_t'] > 0
-    positive_sim = simulation['Y_t'][positive_index]
+    positive_index = simulation["Y_t"] > 0
+    positive_sim = simulation["Y_t"][positive_index]
     MAPE = np.nanmean(abs(y_trace[positive_index] - positive_sim) / positive_sim)
 
     assert mean_error_rate < 0.05
     assert MAPE < 0.05
 
-    return {'mean_error_rate': mean_error_rate, 'MAPE': MAPE}
+    return {"mean_error_rate": mean_error_rate, "MAPE": MAPE}
 
 
 # def test_PriorRobust():
--- tests/utils.py
+++ tests/utils.py
@@ -1,36 +1,34 @@
-import numpy as np
-
-import theano.tensor as tt
-
-import pymc3 as pm
-
 from pymc3_hmm.distributions import HMMStateSeq, SwitchingProcess
 
+import numpy as np
+import theano.tensor as tt
+import pymc3 as pm
 import pandas as pd
+import numbers
 
 
-def simulate_poiszero_hmm(N, mus=np.r_[10.0, 30.0],
-                          pi_0_a=np.r_[1, 1, 1],
-                          Gamma=np.r_['0,2,1',
-                                      [5, 1, 1],
-                                      [1, 3, 1],
-                                      [1, 1, 5]]
-                          ):
+def simulate_poiszero_hmm(
+    N, mus=np.r_[10.0, 30.0], pi_0_a=np.r_[1, 1], Gamma=np.r_["0,2", [5, 1], [1, 3]]
+):
+    if isinstance(mus, numbers.Number):
+        mus = np.r_[mus]
     assert pi_0_a.size == mus.size + 1 == Gamma.shape[0] == Gamma.shape[1]
 
     with pm.Model() as test_model:
-        trans_rows = [pm.Dirichlet(f'p_{i}', r) for i, r in enumerate(Gamma)]
+        trans_rows = [pm.Dirichlet(f"p_{i}", r) for i, r in enumerate(Gamma)]
         P_tt = tt.stack(trans_rows)
-        P_rv = pm.Deterministic('P_tt', P_tt)
+        P_rv = pm.Deterministic("P_tt", P_tt)
 
-        pi_0_tt = pm.Dirichlet('pi_0', pi_0_a)
+        pi_0_tt = pm.Dirichlet("pi_0", pi_0_a)
 
-        S_rv = HMMStateSeq('S_t', N, P_rv, pi_0_tt)
+        S_rv = HMMStateSeq("S_t", N, P_rv, pi_0_tt)
 
-        Y_rv = SwitchingProcess('Y_t',
-                                [pm.Constant.dist(0)] + [pm.Poisson.dist(mu)
-                                                         for mu in mus],
-                                S_rv, observed=np.zeros(N))
+        Y_rv = SwitchingProcess(
+            "Y_t",
+            [pm.Constant.dist(0)] + [pm.Poisson.dist(mu) for mu in mus],
+            S_rv,
+            observed=np.zeros(N),
+        )
 
         y_test_point = pm.sample_prior_predictive(samples=1)
 
@@ -38,7 +36,7 @@ def simulate_poiszero_hmm(N, mus=np.r_[10.0, 30.0],
 
 
 def time_series(N):
-    t = pd.date_range(end=pd.to_datetime('today'), periods=N, freq='H')
+    t = pd.date_range(end=pd.to_datetime("today"), periods=N, freq="H")
     # month = pd.get_dummies(t.month)
     week = pd.get_dummies(t.weekday).values
     hour = pd.get_dummies(t.hour).values
@@ -59,15 +57,18 @@ def gen_defualt_params_seaonality(N):
 
     seasonal = tt.dot(time_series(N), betas)
 
-    return {"N": N, "mus": np.r_[3000.0 * seasonal, 1000.0 * seasonal],
-            "pi_0_a": np.r_[1, 1, 1],
-            'Gamma': np.r_['0,2,1', [10, 1, 5], [1, 10, 5], [5, 1, 20]],
-            }
+    return {
+        "N": N,
+        "mus": np.r_[3000.0 * seasonal, 1000.0 * seasonal],
+        "pi_0_a": np.r_[1, 1, 1],
+        "Gamma": np.r_["0,2,1", [10, 1, 5], [1, 10, 5], [5, 1, 20]],
+    }
 
 
 def gen_defualt_param(N):
-    return {"N": N,
-            "mus": np.r_[5000, 7000],
-            "pi_0_a": np.r_[1, 1, 1],
-            'Gamma': np.r_['0,2,1', [5, 1, 1], [1, 3, 1], [1, 1, 5]],
-            }
+    return {
+        "N": N,
+        "mus": np.r_[5000, 7000],
+        "pi_0_a": np.r_[1, 1, 1],
+        "Gamma": np.r_["0,2,1", [5, 1, 1], [1, 3, 1], [1, 1, 5]],
+    }
