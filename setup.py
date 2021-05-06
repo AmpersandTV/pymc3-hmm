ff628b7ffec3cd769001ae02e0a93a9cd3c64e26
Shi Fan <shi.fan@ampersand.tv>
2021-05-06 18:49:27 -0400

    Update setup.py aesara import
    Co-authored-by: Brandon T. Willard <971601+brandonwillard@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ setup(
         "numpy>=1.18.1",
         "scipy>=1.4.0",
         "pymc3==3.11.2",
-        "theano-pymc>=1.1.0",
-        "aesara==2.0.7",
     ],
     tests_require=["pytest"],
     long_description=open("README.md").read() if exists("README.md") else "",
